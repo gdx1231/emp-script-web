7f9f402dce1b62900c012dd762069525bd70831f
Guolei <lei.guo@gdxsoft.com>
2022-11-10 18:33:34 +0800

    qrcode

---
+++ src/main/java/com/gdxsoft/web/qrcode/QRCodeWeb.java
@@ -0,0 +1,313 @@
+package com.gdxsoft.web.qrcode;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.json.JSONObject;
+
+import com.gdxsoft.easyweb.script.RequestValue;
+import com.gdxsoft.easyweb.utils.UConvert;
+import com.gdxsoft.easyweb.utils.UFile;
+import com.gdxsoft.easyweb.utils.UJSon;
+import com.gdxsoft.easyweb.utils.UPath;
+import com.gdxsoft.easyweb.utils.Utils;
+import com.gdxsoft.web.shortUrl.ShortUrl;
+import com.gdxsoft.web.shortUrl.UrlShort;
+
+public class QRCodeWeb {
+	private RequestValue rv;
+	private String hostPrefix; // 短地址前缀，例如 https://gdxsoft.xyz/s/
+
+	public QRCodeWeb(RequestValue rv, String hostPrefix) {
+		this.rv = rv;
+		this.hostPrefix = hostPrefix;
+	}
+
+	public File getLogo(String logo) throws Exception {
+		if (logo.indexOf("./") >= 0 || logo.indexOf(".\\") >= 0 || logo.indexOf("..") >= 0) {
+			throw new Exception("Invalid logo, NOT alow ./ or .. ");
+		}
+
+		String logo_path = UPath.getPATH_UPLOAD() + "/" + logo;
+		File fileLogo = new File(logo_path);
+		if (!fileLogo.exists()) {
+			throw new Exception("Logo not found: " + logo);
+		}
+
+		return fileLogo;
+	}
+
+	private boolean checkIfNoneMatch(String md5) {
+		String IfNoneMatch = rv.getRequest().getHeader("If-None-Match");
+		if (IfNoneMatch != null) {
+			if (("W/" + md5).equals(IfNoneMatch)) {
+				// 304 文件没有修改
+				is304 = true;
+				return true;
+			}
+		}
+		is304 = false;
+		return false;
+	}
+
+	public JSONObject createQRCode(String msg, int width, boolean useShortUrl, boolean onlyNumber, byte[] logoBuffer) {
+		JSONObject obj = new JSONObject();
+		UJSon.rstSetTrue(obj, null);
+		obj.put("original_msg", msg);
+
+		if (width > 1000) {
+			width = 1000;
+		} else if (width < 100) {
+			width = 100;
+		}
+		obj.put("width", width);
+
+		// 拼接参数，用于判断是否已经创建
+		StringBuilder hash_string = new StringBuilder();
+		hash_string.append("msg=");
+		hash_string.append(msg);
+		hash_string.append("~~~~width=");
+		hash_string.append(width);
+		hash_string.append("~~~~logo=");
+		hash_string.append(Utils.md5(logoBuffer));
+
+		if (rv.s("use_short_url") != null) { // 使用短地址
+			hash_string.append("~~~~use_short_url=yes");
+		}
+
+		// 获取参数的md5
+		String md5 = Utils.md5(hash_string.toString());
+		this.hashString = hash_string.toString();
+		obj.put("md5", md5);
+		this.md5 = md5;
+
+		boolean ifNoneMatch = this.checkIfNoneMatch(md5);
+		if (ifNoneMatch) {
+			// 304 文件没有修改
+			obj.put("httpcode", 304);
+			return obj;
+		}
+
+		// 二维码 Qcode编码
+		byte[] img = null;
+		String[] paths = QRCode.getQRCodeSavedPath(md5, "jpeg");
+		String path = paths[0];
+		File exists = new File(path);
+		if (exists.exists()) {
+			try {
+				img = UFile.readFileBytes(exists.getAbsolutePath());
+			} catch (Exception err) {
+
+			}
+		}
+
+		if (img == null) {
+			if (useShortUrl) { // 创建段地址
+				msg = this.createShortUrl(msg, onlyNumber, rv);
+			}
+			try {
+				img = QRCode.createQRCode(msg, width, logoBuffer);
+			} catch (IOException e) {
+				UJSon.rstSetFalse(obj, e.getMessage());
+				return obj;
+			}
+
+			try {
+				UFile.createBinaryFile(path, img, true);
+			} catch (Exception e) {
+				UJSon.rstSetFalse(obj, e.getMessage());
+				return obj;
+			}
+		}
+
+		this.setQrImage(img);
+		this.qrImageUrl = paths[1];
+
+		obj.put("msg", msg);
+		obj.put("img", paths[1]);
+		UJSon.rstSetTrue(obj, null);
+
+		return obj;
+	}
+
+	/**
+	 * 创建二维码
+	 * 
+	 * @param msg         消息
+	 * @param width       宽带
+	 * @param useShortUrl 是否用短地址
+	 * @param logo        中间的小图
+	 * @return
+	 */
+	public JSONObject createQRCode(String msg, int width, boolean useShortUrl, boolean onlyNumber, String logo) {
+		JSONObject obj = new JSONObject();
+		UJSon.rstSetTrue(obj, null);
+		obj.put("original_msg", msg);
+
+		if (width > 1000) {
+			width = 1000;
+		} else if (width < 100) {
+			width = 100;
+		}
+		obj.put("width", width);
+
+		// 拼接参数，用于判断是否已经创建
+		StringBuilder hash_string = new StringBuilder();
+		hash_string.append("msg=");
+		hash_string.append(msg);
+		hash_string.append("~~~~width=");
+		hash_string.append(width);
+
+		if (logo == null) {
+			logo = "";
+		} else {
+			logo = logo.trim();
+		}
+
+		File fileLogo = null;
+
+		if (logo.length() > 0) {
+			hash_string.append("~~~~logo=");
+			try {
+				fileLogo = this.getLogo(logo);
+			} catch (Exception e) {
+				UJSon.rstSetFalse(obj, e.getMessage());
+				return obj;
+			}
+			hash_string.append(fileLogo.getAbsolutePath());
+		}
+		if (rv.s("use_short_url") != null) { // 使用短地址
+			hash_string.append("~~~~use_short_url=yes");
+		}
+		obj.put("logo", logo);
+
+		// 获取参数的md5
+		String md5 = Utils.md5(hash_string.toString());
+		this.hashString = hash_string.toString();
+		this.md5 = md5;
+		obj.put("md5", md5);
+
+		boolean ifNoneMatch = this.checkIfNoneMatch(md5);
+		if (ifNoneMatch) {
+			// 304 文件没有修改
+			obj.put("httpcode", 304);
+			return obj;
+		}
+
+		// 二维码 Qcode编码
+		byte[] img = null;
+		String[] paths = QRCode.getQRCodeSavedPath(md5, "jpeg");
+		String path = paths[0];
+		File exists = new File(path);
+		if (exists.exists()) {
+			try {
+				img = UFile.readFileBytes(exists.getAbsolutePath());
+			} catch (Exception err) {
+
+			}
+		}
+
+		if (img == null) {
+			if (useShortUrl) { // 创建段地址
+				msg = this.createShortUrl(msg, onlyNumber, rv);
+			}
+			if (logo.length() == 0) {
+				img = QRCode.createQRCode(msg, width);
+			} else {
+				try {
+					img = QRCode.createQRCode(msg, width, fileLogo);
+				} catch (IOException e) {
+					UJSon.rstSetFalse(obj, e.getMessage());
+					return obj;
+				}
+			}
+			try {
+				UFile.createBinaryFile(path, img, true);
+			} catch (Exception e) {
+				UJSon.rstSetFalse(obj, e.getMessage());
+				return obj;
+			}
+		}
+		this.setQrImage(img);
+		this.qrImagePath = path;
+		this.qrImageUrl = paths[1];
+		obj.put("msg", msg);
+		obj.put("img", paths[1]);
+		UJSon.rstSetTrue(obj, null);
+
+		return obj;
+	}
+
+	public String createShortUrl(String longUrl, boolean onlyNumber, RequestValue rv) {
+		UrlShort us;
+		List<UrlShort> al = ShortUrl.getUrls(longUrl);
+		if (al.size() == 0) {
+			ShortUrl su = new ShortUrl();
+			Integer supId = rv.isNotNull("g_sup_id") ? rv.getInt("g_sup_id") : null;
+			Integer admId = rv.isNotNull("g_adm_Id") ? rv.getInt("g_adm_Id") : null;
+			us = su.addUrl(longUrl, supId, admId, onlyNumber);
+		} else {
+			us = al.get(0);
+		}
+
+		return hostPrefix + us.getUrlUid();
+	}
+
+	public Map<String, String> getResponseHeaders(int cacheLife) {
+		Map<String, String> headers = new HashMap<String, String>();
+		// int cache_life = 3000;//3000s
+		headers.put("ETag", "W/" + md5);
+		headers.put("Cache-Control", "max-age=" + cacheLife);
+		return headers;
+	}
+
+	public String toImageBase64() {
+		if (this.qrImage == null) {
+			return null;
+		}
+		return "data:image/jpeg;base64," + UConvert.ToBase64String(qrImage);
+	}
+
+	private byte[] qrImage;
+
+	public byte[] getQrImage() {
+		return qrImage;
+	}
+
+	void setQrImage(byte[] qrImage) {
+		this.qrImage = qrImage;
+	}
+
+	private String qrImagePath;
+
+	public String getQrImagePath() {
+		return qrImagePath;
+	}
+
+	private String md5;
+
+	public String getMd5() {
+		return md5;
+	}
+
+	private String hashString;
+
+	public String getHashString() {
+		return hashString;
+	}
+
+	private boolean is304;
+
+	public boolean is304() {
+		return this.is304;
+	}
+
+	private String qrImageUrl;
+
+	public String getQrImageUrl() {
+		return qrImageUrl;
+	}
+}
--- src/main/java/com/gdxsoft/web/shortUrl/ShortUrl.java
+++ src/main/java/com/gdxsoft/web/shortUrl/ShortUrl.java
@@ -6,14 +6,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import com.gdxsoft.easyweb.utils.USnowflake;
 import com.gdxsoft.easyweb.utils.Utils;
 
 /**
  * 短地址
- * 
- * @author admin
- *
  */
 public class ShortUrl {
 
@@ -60,43 +56,33 @@ public class ShortUrl {
 	 */
 	public static ArrayList<UrlShort> getUrls(String url, int admId) {
 		String md5 = Utils.md5(url);
-		return getUrlsByMd5(md5, admId);
-	}
-
-	public static ArrayList<UrlShort> getUrlsByMd5(String md5, int admId) {
 		UrlShortDao d1 = new UrlShortDao();
-		String where = " URL_MD5 = '" + md5 + "' and adm_id=" + admId + " and url_status='USED' order by URL_ID desc";
+		String where = " URL_MD5 = '" + md5 + "' and adm_id=" + admId + " order by URL_ID desc";
 		return d1.getRecords(where);
 	}
 
 	/**
-	 * 添加URL
+	 * 添加URL，返回数字和文字短地址
 	 * 
 	 * @param url
 	 * @param supId
 	 * @param admId
 	 * @return
 	 */
-	public UrlShort addUrl(String url, int supId, int admId) {
-		String md5 = Utils.md5(url);
+	public UrlShort addUrl(String url, Integer supId, Integer admId) {
 		UrlShortDao d1 = new UrlShortDao();
-		ArrayList<UrlShort> al = ShortUrl.getUrlsByMd5(md5, admId);
-
-		if (al.size() > 0) {
-			return al.get(0);
-		}
 
 		UrlShort o = new UrlShort();
-		o.setUrlId(USnowflake.nextId());
 
 		o.setAdmId(admId);
 		o.setSupId(supId);
 		o.setUrlCdate(new Date());
 		o.setUrlFull(url);
 		o.setUrlStatus("USED");
-
 		String paraUrlUid = this.createUid();
 		o.setUrlUid(paraUrlUid);
+
+		String md5 = Utils.md5(url);
 
 		o.setUrlMd5(md5);
 
@@ -106,17 +92,57 @@ public class ShortUrl {
 	}
 
 	/**
-	 * 创建唯一编号
+	 * 添加URL
+	 * 
+	 * @param url
+	 * @param supId
+	 * @param admId
+	 * @param onlyNumber 是否仅数字
+	 * @return
+	 */
+	public UrlShort addUrl(String url, Integer supId, Integer admId, boolean onlyNumber) {
+		UrlShortDao d1 = new UrlShortDao();
+
+		UrlShort o = new UrlShort();
+
+		o.setAdmId(admId);
+		o.setSupId(supId);
+		o.setUrlCdate(new Date());
+		o.setUrlFull(url);
+		o.setUrlStatus("USED");
+		String paraUrlUid = this.createUid(onlyNumber);
+		o.setUrlUid(paraUrlUid);
+
+		String md5 = Utils.md5(url);
+
+		o.setUrlMd5(md5);
+
+		d1.newRecord(o);
+
+		return o;
+	}
+
+	/**
+	 * 创建唯一编号，数字和文字
 	 * 
 	 * @return
 	 */
 	public String createUid() {
+
+		return createUid(false);
+	}
+
+	/**
+	 * 创建唯一编号
+	 * 
+	 * @param onlyNumber 是否仅仅是数字
+	 * @return
+	 */
+	public String createUid(boolean onlyNumber) {
 		int inc = 0;
 		String uid = null;
-		int len[] = { 4, 5, 6, 7, 8, 9, 10 };
 		while (uid == null) {
-			int codeLen = inc < len.length ? len[inc] : 10;
-			uid = this.chcekAndGetUnique(codeLen);
+			uid = this.chcekAndGetUnique(onlyNumber);
 			inc++;
 			if (inc > 10) {
 				return "太多次的尝试";
@@ -126,32 +152,92 @@ public class ShortUrl {
 	}
 
 	/**
+	 * 利用随机数生成字符串
+	 * 
+	 * @param length 生成的长度
+	 * 
+	 * @return 随机数生成字符串
+	 */
+	public String randomNumber(int length) {
+		StringBuilder sb = new StringBuilder();
+		char[] CHARS = "0123456789".toCharArray();
+
+		int max = CHARS.length;
+		for (int i = 0; i < length; i++) {
+			String a = Math.random() * max + "";
+			int b = Integer.parseInt(a.split("\\.")[0]);
+			char c = CHARS[b];
+			sb.append(c);
+		}
+		return sb.toString();
+	}
+
+	private String getRandomCode(int length, boolean onlyNumber) {
+		return onlyNumber ? randomNumber(length) : Utils.randomStr(length);
+	}
+
+	/**
 	 * 获取唯一的值
 	 * 
 	 * @return
 	 */
-	private String chcekAndGetUnique(int codeLen) {
-		Map<String, Boolean> unids = new HashMap<String, Boolean>();
-		for (int i = 0; i < 100; i++) {
-			String uid = Utils.randomStr(codeLen);
-			unids.put(uid, true);
-		}
-
-		UrlShortDao d1 = new UrlShortDao();
-		StringBuilder sb = new StringBuilder();
-		sb.append(" URL_UID in (");
-		int inc = 0;
+	private String chcekAndGetUnique(boolean onlyNumber) {
+		List<String> unids = new ArrayList<String>();
 		String firstUnid = null;
-		for (String unid : unids.keySet()) {
-			if (inc == 0) {
-				firstUnid = unid;
+
+		int inc = 0;
+		StringBuilder sb = new StringBuilder();
+		sb.append(" URL_STATUS='USED' AND URL_UID in (");
+
+		for (int i = 0; i < 50; i++) {
+			String uid = this.getRandomCode(4, onlyNumber);
+			firstUnid = uid;
+			unids.add(uid);
+			if (i == 0) {
+				firstUnid = uid;
 			} else {
 				sb.append("\n	, ");
 			}
-			sb.append("'" + unid + "'");
-			inc++;
+			sb.append("'" + uid + "'");
+		}
+		for (int i = 0; i < 40; i++) {
+			String uid = this.getRandomCode(5, onlyNumber);
+			unids.add(uid);
+			sb.append("\n	, ");
+			sb.append("'" + uid + "'");
+		}
+		for (int i = 0; i < 20; i++) {
+			String uid = this.getRandomCode(6, onlyNumber);
+			unids.add(uid);
+			sb.append("\n	, ");
+			sb.append("'" + uid + "'");
+		}
+		for (int i = 0; i < 20; i++) {
+			String uid = this.getRandomCode(7, onlyNumber);
+			unids.add(uid);
+			sb.append("\n	, ");
+			sb.append("'" + uid + "'");
+		}
+		for (int i = 0; i < 20; i++) {
+			String uid = this.getRandomCode(8, onlyNumber);
+			unids.add(uid);
+			sb.append("\n	, ");
+			sb.append("'" + uid + "'");
+		}
+		for (int i = 0; i < 20; i++) {
+			String uid = this.getRandomCode(9, onlyNumber);
+			unids.add(uid);
+			sb.append("\n	, ");
+			sb.append("'" + uid + "'");
+		}
+		for (int i = 0; i < 20; i++) {
+			String uid = this.getRandomCode(10, onlyNumber);
+			unids.add(uid);
+			sb.append("\n	, ");
+			sb.append("'" + uid + "'");
 		}
 		sb.append(")");
+		UrlShortDao d1 = new UrlShortDao();
 		String whereString = sb.toString();
 		List<String> fields = new ArrayList<String>();
 		fields.add("URL_ID");
@@ -163,17 +249,20 @@ public class ShortUrl {
 		if (exists.size() == inc) {
 			return null;
 		}
-
+		if (exists.size() == unids.size()) {
+			return null;
+		}
+		Map<String, Boolean> map = new HashMap<>();
 		for (int i = 0; i < exists.size(); i++) {
 			UrlShort o1 = exists.get(i);
-			if (unids.containsKey(o1.getUrlUid())) {
-				unids.remove(o1.getUrlUid());
+			map.put(o1.getUrlUid(), true);
+		}
+		for (int i = 0; i < unids.size(); i++) {
+			String uid = unids.get(i);
+			if (!map.containsKey(uid)) {
+				return uid;
 			}
 		}
-		for (String unid : unids.keySet()) {
-			return unid;
-		}
-
 		return null;
 
 	}
